a94db287a24b46b6dc9960731394db14f9ff9481
Nabeel Nasir <nabeeln7@gmail.com>
2022-12-02 15:07:40 -0500

    add fft and cupy matrix multiplication tasks

---
+++ executor/flexecutor/tasker/fft.py
@@ -0,0 +1,11 @@
+import sys
+import numpy as np
+
+def run_fft_task_cpu(signal):
+    return np.fft.fft(signal)
+
+if __name__ == '__main__':
+    # audio is sampled usually at 22050 or 44100 samples per sec
+    signal_size = int(sys.argv[1])
+    a = np.random.normal(0, 0.5, size=signal_size)
+    print(run_fft_task_cpu(a))
